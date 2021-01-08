2f3cfcd36338b30386093d1c89ff253c2551c86a
TalyukaS <stas65@mail.ru>
2021-01-08 16:35:09 +0400

    Dobavlen comment v Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class Main {
 
     public static void main(String[] args) {
-
+        // Добавление записи в базу
         Db db = new Db();
         try {
             db.insertArticles("5 Статья", "А это пятая статья самого Стаса", "9.01.2021", "Stas");
